6bb2274b0d56b5bab74b51fc377874521b05433f
Talal Ahmed <talalsidd31@gmail.com>
2024-01-27 00:59:35 +0500

    updating code

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
 import { useState, useEffect } from "react";
-import {uuid} from 'uuidv4';
+import { v4 as uuid} from 'uuid'
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
-import AddContact from "./components/Addcontact";
+import AddContact from "./components/AddContact";
 import ContactList from "./components/ContactList";
 
 function App() {
   const LOCAL_STORAGE_KEY = "contacts";
 
-  const [contacts, setContacts] = useState([]);
-  
+  const [contacts, setContacts] = useState(JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) ?? []);
   const addContactHandler = (contact) => {
     console.log(contact);
-    setContacts([...contacts, contact]);
+    setContacts([...contacts, {id: uuid(), ...contacts}]);
   };
   
-  useEffect(() => {
-   const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
-   if(retriveContacts)setContacts(retriveContacts);
-  }, []);
+  // useEffect(() => {
+  //  const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+  //  if(retriveContacts)setContacts(retriveContacts);
+  // }, []);
 
   useEffect(() => {
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
