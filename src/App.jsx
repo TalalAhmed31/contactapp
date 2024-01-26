f3bb3819332ed55e5864fcee549a37b354662899
Talal Ahmed <talalsidd31@gmail.com>
2024-01-27 01:49:19 +0500

    react router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { v4 as uuid} from 'uuid'
+import { v4 as uuid } from "uuid";
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
@@ -9,30 +9,31 @@ import ContactList from "./components/ContactList";
 function App() {
   const LOCAL_STORAGE_KEY = "contacts";
 
-  const [contacts, setContacts] = useState(JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) ?? []);
+  const [contacts, setContacts] = useState(
+    JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) ?? []
+  );
   const addContactHandler = (contact) => {
     console.log(contact);
-    setContacts([...contacts, {id: uuid(), ...contact}]);
+    setContacts([...contacts, { id: uuid(), ...contact }]);
   };
 
   const removeContactHandler = (id) => {
     const newContactList = contacts.filter((contact) => {
       return contact.id !== id;
-    })
+    });
 
     setContacts(newContactList);
-  }
+  };
 
   useEffect(() => {
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
   }, [contacts]);
 
-  
   return (
     <div className="ui container">
       <Header />
       <AddContact addContactHandler={addContactHandler} />
-      <ContactList contacts={contacts} getContactId = {removeContactHandler}/>
+      <ContactList contacts={contacts} getContactId={removeContactHandler} />
     </div>
   );
 }
