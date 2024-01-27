bf9409bc53b55035b2996c7ccb6de012a85cdf31
Talal Ahmed <talalsidd31@gmail.com>
2024-01-28 02:18:37 +0500

    final contact app

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -6,7 +6,15 @@ const ContactList = (props) =>{
     const deleteContactHandler = (id)=>{
         props.getContactId(id);
     };
-    const renderContactList = props.contacts.map((contact) => {
+
+    const contacts = [
+        {
+        id : "1",
+        name: "Talal",
+        email: "tala@gmail.com"
+    },
+];
+    const renderContactList = contacts.map((contact) => {
         return(
             <ContactCard contact={contact} clickHander ={deleteContactHandler} key={contact.id}/>
         )
