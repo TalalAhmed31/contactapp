5d037b2dc2e84e291ba2cc86b4209f1d811d5fad
Talal Ahmed <talalsidd31@gmail.com>
2024-01-28 19:16:51 +0500

    adding the AddContact.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { v4 as uuid } from "uuid";
 import React from "react";
 import "./App.css";
 import Header from "./components/Header.jsx";
-import AddContact from "./components/AddContact.jsx";
+import AddContact from "./components/Addcontact.jsx";
 import ContactList from "./components/ContactList.jsx";
 
 function App() {
